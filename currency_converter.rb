5d9018fb9eb3ce0a2eb4dbf5395e56545f8ffd3c
John M Buscemi <jmbuscemi@gmail.com>
2015-05-08 17:42:03 -0400

    Initialize hash of currency codes. Add convert method

---
+++ currency_converter.rb
@@ -0,0 +1,20 @@
+class UnknownCurrencyCodeError < StandardError
+end
+
+class CurrencyConverter
+  attr_reader :hash
+
+  def initialize(hash)
+    @conversion_rates = hash
+  end
+
+  def convert(currency, code)
+    if !@conversion_rates.include?(currency.code) || !@conversion_rates.include?(code)
+      raise UnknownCurrencyCodeError, "Unknown currency code!!"
+    else
+      Currency.new(currency.amount * @conversion_rates[code], code)
+    end
+  end
+
+
+end
