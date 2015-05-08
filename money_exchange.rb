6af158d7b724e67d158763f76c00d555a9d08219
John M Buscemi <jmbuscemi@gmail.com>
2015-05-08 11:39:35 -0400

    Initial program commit - create objects in Currency class

---
+++ money_exchange.rb
@@ -0,0 +1,12 @@
+require "./currency.rb"
+require "./currency_converter.rb"
+
+john_money = Currency.new(10, "USD")
+jim_money = Currency.new(10, "EUR")
+
+
+puts "John has #{john_money.amount} #{john_money.code}."
+puts "Joe has #{jim_money.amount} #{jim_money.code}."
+
+
+puts john_money == jim_money
