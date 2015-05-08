4b71060fa4689c5f4eda6e8ef42bc1606edfbbe7
John M Buscemi <jmbuscemi@gmail.com>
2015-05-08 15:28:34 -0400

    Add test for $, €, £ input

--- money_exchange.rb
+++ money_exchange.rb
@@ -1,9 +1,10 @@
 require "./currency.rb"
 require "./currency_converter.rb"
 
-john_money = Currency.new(10, "USD")
-jim_money = Currency.new(15, "USD")
-new_money = Currency.new(23, "USD")
+john_money = Currency.new("USD", 10)
+jim_money = Currency.new("USD", 15)
+new_money = Currency.new("$ 1.25")
+string_test = Currency.new("€ 1000.43")
 
 puts "*******Create Currency objects*******"
 puts "John has #{john_money.amount} #{john_money.code}."
@@ -31,3 +32,7 @@ puts "*******Multiply Currency*******"
 converted_money = john_money * (2.5)
 puts "You now have #{converted_money.amount}"
 puts ""
+
+puts "*******Allow for $ Input*******"
+p string_test.code
+p string_test.amount
