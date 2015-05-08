9b06f89a40c214dc9d094fa8758da4f86ec7e75c
John M Buscemi <jmbuscemi@gmail.com>
2015-05-08 13:38:21 -0400

    Include add/subtract method

--- money_exchange.rb
+++ money_exchange.rb
@@ -2,11 +2,17 @@ require "./currency.rb"
 require "./currency_converter.rb"
 
 john_money = Currency.new(10, "USD")
-jim_money = Currency.new(10, "EUR")
+jim_money = Currency.new(15, "USD")
+new_money = Currency.new(23, "USD")
 
 
 puts "John has #{john_money.amount} #{john_money.code}."
 puts "Joe has #{jim_money.amount} #{jim_money.code}."
 
+puts john_money == jim_money #true if amount/currency are same
 
-puts john_money + jim_money
+total_money = john_money + new_money
+puts "You now have #{total_money.amount} #{total_money.code}"
+
+less_money = john_money - new_money
+puts "You now have #{less_money.amount} #{less_money.code}"
