1fa20f4f081b86d7429f8e54e3fe0d3b9857ca8d
John M Buscemi <jmbuscemi@gmail.com>
2015-05-08 17:43:41 -0400

    Run conversion for 1USD

--- money_exchange.rb
+++ money_exchange.rb
@@ -1,38 +1,46 @@
 require "./currency.rb"
 require "./currency_converter.rb"
 
-john_money = Currency.new("USD", 10)
-jim_money = Currency.new("USD", 15)
-new_money = Currency.new("$ 1.25")
-string_test = Currency.new("€ 1000.43")
+currency_converter = CurrencyConverter.new({USD: 1.00000, EUR: 0.89252,
+                                         GBP: 0.64726, JPY: 119.733})
 
-puts "*******Create Currency objects*******"
-puts "John has #{john_money.amount} #{john_money.code}."
-puts "Joe has #{jim_money.amount} #{jim_money.code}."
-puts ""
+p currency_converter.convert(Currency.new(1, :USD), :USD) == Currency.new(1, :USD)
 
-puts "*******Test Currency Equlatiy*******"
-puts john_money == jim_money #true if amount/currency are same
-puts ""
 
-#Add money
-puts "*******Add Currency objects*******"
-total_money = john_money + new_money
-puts "You now have #{total_money.amount} #{total_money.code}"
-puts ""
 
-puts "*******Subtract Currency objects*******"
-#Subtract money
-less_money = john_money - new_money
-puts "You now have #{less_money.amount} #{less_money.code}"
-puts ""
-
-puts "*******Multiply Currency*******"
-#Multiply
-converted_money = john_money * (2.5)
-puts "You now have #{converted_money.amount}"
-puts ""
-
-puts "*******Allow for $ Input*******"
-p string_test.code
-p string_test.amount
+########################################################
+#### Test Currency Object requirements
+# john_money = Currency.new(10, :USD)
+# jim_money = Currency.new(10, :USD)
+# new_money = Currency.new("$ 1.25")
+# string_test = Currency.new("€ 1000.43")
+# puts "*******Create Currency objects*******"
+# puts "John has #{john_money.amount} #{john_money.code}."
+# puts "Joe has #{jim_money.amount} #{jim_money.code}."
+# puts ""
+#
+# puts "*******Test Currency Equality*******"
+# puts john_money == jim_money #true if amount/currency are same
+# puts ""
+#
+# #Add money
+# puts "*******Add Currency objects*******"
+# total_money = john_money + new_money
+# puts "You now have #{total_money.amount} #{total_money.code}"
+# puts ""
+#
+# puts "*******Subtract Currency objects*******"
+# #Subtract money
+# less_money = john_money - new_money
+# puts "You now have #{less_money.amount} #{less_money.code}"
+# puts ""
+#
+# puts "*******Multiply Currency*******"
+# #Multiply
+# converted_money = john_money * (2.5)
+# puts "You now have #{converted_money.amount}"
+# puts ""
+#
+# puts "*******Allow for $ Input*******"
+# p string_test.code
+# p string_test.amount
